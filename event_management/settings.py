700e31ecc7f6a47d6a576f6ba235562247d84332
Tazul Islam <linkedin52425@gmail.com>
2025-02-25 08:52:02 +0600

    done

--- event_management/settings.py
+++ event_management/settings.py
@@ -33,12 +33,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'events',
     'users',
-    'core',
-    "debug_toolbar"
+    'core'
 ]
 
 MIDDLEWARE = [
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -46,10 +44,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
-INTERNAL_IPS = [
-    "127.0.0.1",
 ]
 
 
--- event_management/urls.py
+++ event_management/urls.py
@@ -1,6 +1,5 @@
 
 from django.contrib import admin
-from debug_toolbar.toolbar import debug_toolbar_urls
 from django.urls import path, include
 from core.views import home, no_permission
 from django.conf.urls.static import static
@@ -12,6 +11,6 @@ urlpatterns = [
     path('events/', include('events.urls')),
     path('users/', include('users.urls')),
     path('no_permission/', no_permission, name='no-permission')
-]+ debug_toolbar_urls()
+]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
