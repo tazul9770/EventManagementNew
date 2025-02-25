3b7c3f754d55a6ca0cb3dab62b1337922d42a1d5
Tazul Islam <linkedin52425@gmail.com>
2025-02-25 10:49:50 +0600

    git commit done

--- event_management/settings.py
+++ event_management/settings.py
@@ -157,7 +157,7 @@ EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
-FRONTEND_URL = 'http://127.0.0.1:8000'
+FRONTEND_URL = 'https://eventmanagementnew.onrender.com/'
 
 
 
